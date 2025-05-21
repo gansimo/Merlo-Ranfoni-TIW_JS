27e898123be213a7cc528103106e56367574e960
Simone <gansimo@gmail.com>
2025-05-21 19:34:50 +0200

    cleaned up some classes

--- src/main/java/it/polimi/tiw/daos/CourseDAO.java
+++ src/main/java/it/polimi/tiw/daos/CourseDAO.java
@@ -16,24 +16,6 @@ public class CourseDAO{
 		this.con = connection;
 	}
 	
-	public List<Exam> findExams(int courseID, int profID) throws SQLException {
-		List<Exam> exams = new ArrayList<Exam>();
-		String query = "SELECT a.data FROM Appello AS a JOIN Corso AS c ON a.id_corso = c.id WHERE a.id_corso = ? AND c.id_prof = ?;";
-		try (PreparedStatement pstatement = con.prepareStatement(query)) {
-	        pstatement.setInt(1, courseID);
-	        pstatement.setInt(2, profID);
-	        
-	        try (ResultSet result = pstatement.executeQuery()) {
-	            while (result.next()) {
-	                Exam exam = new Exam();
-	                //exam.setDate(result.getDate("data").toLocalDate());
-	                exam.setStringDate(result.getString("data"));
-	                exams.add(exam);
-	            }
-	        }
-	    }
-		return exams;
-	}
 	
 	public List<Exam> findStudentExams(int courseID, int studID) throws SQLException {
 		List<Exam> exams = new ArrayList<Exam>();
